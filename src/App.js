cefeb5fc32d94eb2016a36f30e513562c68a2de9
leonso <leonziyo@gmail.com>
2020-01-16 20:15:17 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import useApi from './useApi';
 
 function App() {
+    const { response, error, isLoading, refetch } = useApi('http://localhost:4444/orders');
+    console.log({ response, error, isLoading });
+
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
+
+    const onRefresh = () => {
+        refetch();
+    };
+
     return (
         <div>
-            <h1>Hola</h1>
+            {error && (
+                <>
+                    <h1>Error</h1>
+                    <p>{error}</p>
+                </>
+            )}
+            <h1>Data</h1>
+
+            <ul>
+                {response.map((order) => (
+                    <li key={order.id}>{order.name}</li>
+                ))}
+            </ul>
+
+            <button onClick={onRefresh}>Refresh</button>
         </div>
     );
 }
---
+++ src/useApi.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const useApi = (url, _options) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState('');
+
+    const doFetch = async (options = _options) => {
+        try {
+            const response = await fetch(url, options);
+
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+
+            const data = await response.json();
+            setData(data);
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        doFetch();
+    }, []);
+
+    return {
+        response: data,
+        error,
+        isLoading,
+        refetch: doFetch,
+    };
+};
+
+export default useApi;
