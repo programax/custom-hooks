3d27eea22a896fad608d21e0072f6eb07499db18
leonso <leonziyo@gmail.com>
2020-01-19 17:23:55 -0600

    v2

--- src/app2.js
+++ src/app2.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useForm from './useForm';
+
+const initialValues = {
+    email: 'hello@gmail.com',
+    password: '123',
+    isOk: false,
+    gender: 'female',
+    car: 'ford',
+};
 
 function App() {
+    const form = useForm({ initialValues });
+    useEffect(() => {
+        if (form.fields.gender === 'male') {
+            form.addField('extra', '');
+        } else {
+            form.removeField('extra');
+        }
+    }, [form.fields.gender]);
+
+    const handleSubmit = (event) => {
+        // form.fields.password === '123'
+        // TODO...
+        event.preventDefault();
+        console.log(form.fields);
+    };
+
+    console.log(form);
+
     return (
         <div>
-            <h1>Data</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <span>Email</span>
-                <input type="email" />
+                <br />
+                <input type="email" {...form.getInput('email')} />
                 <br />
                 <span>Password</span>
-                <input type="password" />
                 <br />
+                <input type="password" {...form.getInput('password')} />
+                <br />
+                <input type="checkbox" {...form.getCheckbox('isOk')} />
                 <span>Is Ok?</span>
-                <input type="checkbox" />
                 <br />
+                <input type="radio" {...form.getRadio('gender', 'male')} />
                 <span>Male</span>
-                <input type="radio" />
                 <br />
+                <input type="radio" {...form.getRadio('gender', 'female')} />
                 <span>Female</span>
-                <input type="radio" />
                 <br />
+                <select {...form.getSelect('car')}>
+                    <option value="audi">audi</option>
+                    <option value="ford">ford</option>
+                    <option value="mercedes">mercedes</option>
+                </select>
+                <br />
+
+                {form.fields.gender === 'male' && (
+                    <input type="text" {...form.getInput('extra')} />
+                )}
+
+                <button>Submit</button>
             </form>
         </div>
     );
--- src/useForm.js
+++ src/useForm.js
@@ -1,7 +1,50 @@
 import { useState } from "react";
 
-const useForm = () => {
+const useForm = ({ initialValues }) => {
+    const [fields, setFields] = useState(initialValues);
 
+    const onChange = (event) => {
+        const { value, name, type, checked } = event.target;
+
+        setFields({ ...fields, [name]: type === 'checkbox' ? checked : value });
+    };
+
+    const addField = (name, value) => {
+        setFields({ ...fields, [name]: value });
+    };
+
+    const removeField = (name) => {
+        const newFields = { ...fields };
+        delete newFields[name];
+        setFields(newFields);
+    };
+
+    return {
+        fields,
+        addField,
+        removeField,
+        getInput: (name) => ({
+            name,
+            value: fields[name],
+            onChange,
+        }),
+        getCheckbox: (name) => ({
+            name,
+            checked: fields[name],
+            onChange,
+        }),
+        getRadio: (name, value) => ({
+            name,
+            value,
+            checked: value === fields[name],
+            onChange,
+        }),
+        getSelect: (name) => ({
+            name,
+            value: fields[name],
+            onChange,
+        }),
+    };
 };
 
 export default useForm;
